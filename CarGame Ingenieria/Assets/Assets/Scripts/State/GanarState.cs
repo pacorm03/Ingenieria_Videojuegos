e6aac0690b17e1946fb370ab3f9342713e51adab
pacorm03 <rodriguezmartinezfrancisco03@gmail.com>
2024-06-15 22:32:50 +0200

    Condiciones de victoria y derrota y fuentes cambiadas

--- CarGame Ingenieria/Assets/Assets/Scripts/State/GanarState.cs
+++ CarGame Ingenieria/Assets/Assets/Scripts/State/GanarState.cs
@@ -1,94 +1,95 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
 public class GanarState : SceneBaseState
 {
-
     GameObject botonVolver;
     Button botonVolverComp;
 
     GameObject botonMenuPrincipal;
     Button botonMenuPrincipalComp;
 
-
-
-
     Scene sceneFinal;
+
+    SceneStateManager context;
 
     public override void EnterState(SceneStateManager scene)
     {
+        context = scene;
         Debug.Log("Estado: Ganar");
-        sceneFinal = SceneManager.GetSceneByBuildIndex(3);
-        if (sceneFinal.isLoaded)
-        {
-            Debug.Log("La escena ya está cargada");
 
+        sceneFinal = SceneManager.GetSceneByBuildIndex(3);
+        if (!sceneFinal.isLoaded)
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+            SceneManager.LoadScene(3);
         }
         else
         {
-            SceneManager.LoadScene(3);
+            InitializeMenu();
         }
-        Debug.Log("Estado: Menu Ganar");
-        
-        //Reanudar
-        if (botonVolver == null)
-        {
-            botonVolver = GameObject.Find("BotonJugar");
-        }
-        if (botonVolver != null)
-        {
-
-            botonVolverComp = botonVolver.GetComponent<Button>();
-            botonVolverComp.onClick.AddListener(() => ReanudarJuego(scene));
-        }
-        //Salir
-        if (botonMenuPrincipal == null)
-        {
-            botonMenuPrincipal = GameObject.Find("BotonMenuPrincipal");
-        }
-        if (botonMenuPrincipal != null)
-        {
-
-            botonMenuPrincipalComp = botonMenuPrincipal.GetComponent<Button>();
-            botonMenuPrincipalComp.onClick.AddListener(() => IrAlMenuPrincipal(scene));
-        }
-
     }
 
     public override void UpdateState(SceneStateManager scene)
     {
+        // Actualización del estado si es necesario
     }
 
     public override void Exit(SceneStateManager scene)
     {
-
-        //Reanudar
         if (botonVolverComp != null)
         {
-            botonVolverComp.onClick.RemoveListener(() => Exit(scene));
+            botonVolverComp.onClick.RemoveListener(ReanudarJuego);
         }
 
-
-        //Salir
         if (botonMenuPrincipalComp != null)
         {
-            botonMenuPrincipalComp.onClick.RemoveListener(() => Exit(scene));
+            botonMenuPrincipalComp.onClick.RemoveListener(IrAlMenuPrincipal);
         }
 
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+        Debug.Log("Saliendo de estado Ganar");
     }
 
-    void ReanudarJuego(SceneStateManager scene)
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-
-        Debug.Log("Saliendo de perder");
-        scene.SetState(new JuegoSceneState());
+        if (scene.buildIndex == 3)
+        {
+            Debug.Log($"La escena {scene.name} ya está cargada");
+            InitializeMenu();
+        }
     }
 
-    void IrAlMenuPrincipal(SceneStateManager scene)
+    void InitializeMenu()
     {
-        Debug.Log("Saliendo de perder");
-        scene.SetState(new MenuInicioState());
+        botonVolver = GameObject.Find("BotonJugar");
+        if (botonVolver != null)
+        {
+            botonVolverComp = botonVolver.GetComponent<Button>();
+            botonVolverComp.onClick.AddListener(ReanudarJuego);
+            Debug.Log("Boton Volver encontrado");
+        }
+
+        botonMenuPrincipal = GameObject.Find("BotonMenuPrincipal");
+        if (botonMenuPrincipal != null)
+        {
+            botonMenuPrincipalComp = botonMenuPrincipal.GetComponent<Button>();
+            botonMenuPrincipalComp.onClick.AddListener(IrAlMenuPrincipal);
+            Debug.Log("Boton Menu Principal encontrado");
+        }
+    }
+
+    void ReanudarJuego()
+    {
+        Debug.Log("Saliendo de ganar");
+        context.SetState(new JuegoSceneState());
+    }
+
+    void IrAlMenuPrincipal()
+    {
+        Debug.Log("Saliendo de ganar");
+        context.SetState(new MenuInicioState());
     }
 }
